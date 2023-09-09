c73effc7dba3506709869bf934007cb5b4d5931a
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-09-09 14:30:11 +0300

    Completed hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,83 +4,84 @@ import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { ImageGalleryItem } from "./ImageGalleryItem/ImageGalleryItem";
 import { Searchbar } from "./Searchbar/Searchbar";
 import { getApi } from "./Search/search";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { Modal } from './Modal/Modal';
 
-export class App extends Component {
-  state = {
-    searchTxt: '',
-    loadMore: 'false',
-    content: [],
-    page: 1,
-    perPage: 12,
-    isLoad: false,
-    isModal: false,
-    imgModal: '',
-    error: false,
-    notFound: false,
-    totalHits: 0, 
-  }
 
 
 
-  componentDidUpdate(_, prevState,) {
-    const { searchTxt, page, perPage, totalHits } = this.state
+export const App = () => {
 
-    if (searchTxt !== prevState.searchTxt || page !== prevState.page) {
-      this.setState({ loadMore: false, isLoad: true })
-      getApi(searchTxt, page, perPage)
-        .then((respons) => {
+  const [searchTxt, setSearchTxt] = useState('');
+  const [loadMore, setLoadMore] = useState(false);
+  const [content, setContent] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoad, setisLoad] = useState(false)
+  const [isModal, setIsModal] = useState(false)
+  const [imgModal, setImgModal] = useState('')
+  const [error, setError] = useState(false)
+  const [notFound, setNotFound] = useState(false)
+  const perPage = 12
+ 
 
-          if (respons.data.hits.length > 0) {
-            this.setState(prevState => ({
-              content: prevState.content.concat(respons.data.hits),
-            }))
-            this.setState({ loadMore: true })
-            this.setState({ notFound: false })
-            this.setState({ totalHits: Math.round(respons.data.totalHits / perPage) - page })
-            if (totalHits === 1) {
-              this.setState({ loadMore: false })
-            }
-          }
-          else {
-            this.setState({ notFound: true })
-          }
+  useEffect(() => {
+    if (searchTxt) {
+      setisLoad(true)
+          getApi(searchTxt, page, perPage)
+            .then((respons) => {
+              const paginationState = (Math.round((respons.data.totalHits / perPage)))
+              if (respons.data.hits.length > 0) {
+                setContent(prevState => prevState.concat(respons.data.hits))
+                setLoadMore(true)
+                setNotFound(false)
+                if (paginationState - page === 1) {
+                  setLoadMore(false)
+                }
+              }
+              else {
+                setNotFound(true)
+                setLoadMore(false)
+              }
+            })
+            .finally(
+              setisLoad(false)
+          ).catch((error) => {
+            setError(true)
+          })
+        }
+  }, [searchTxt, page])
 
-        })
-        .finally(
-          this.setState({ isLoad: false })
-      ).catch((error) => {
-        this.setState({ error: true })
-      })
-    }
-  }
-  
-  onModalWindow = (value) => {
+ 
+
+
+  const onModalWindow = (value) => {
     if (value.target.className === 'open') {
-      this.setState({ isModal: true, imgModal: value.target.alt})
+      setIsModal(true)
+      setImgModal(value.target.alt)
     }
   }
 
-  closeModalWindow = () => {
-    this.setState({ isModal: false, imgModal: '' })
+  const closeModalWindow = () => {
+    setIsModal(false)
+    setImgModal('')
   }
 
-  onSubmit = (searchTxt) => {
+  const onSubmit = (searchTxt) => {
     if (!searchTxt.name) {
       return
     }
-    this.setState(({ searchTxt: searchTxt, content: [], page: 1 }))
-    
+    setSearchTxt(searchTxt)
+    setContent([])
+    setPage(1)
   }
   
-  loadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }))
+  const loadMorePage = () => {
+    setPage(prevState=> prevState +1)
   }
 
-  render() {
-    return (
-      <div
+
+  return (
+          <div
         style={{
           display: 'grid',
           gridTemplateColumns: '1fr',
@@ -88,46 +89,46 @@ export class App extends Component {
           paddingBottom: '24px'
       }}
       >
-        {this.state.isModal && (
+        {isModal && (
           <Modal
-            imgModal={this.state.imgModal}
-            closeModalWindow={this.closeModalWindow}
+            imgModal={imgModal}
+            closeModalWindow={closeModalWindow}
           ></Modal>)}
         
         <Searchbar
-          onSubmit={this.onSubmit}></Searchbar>
+          onSubmit={onSubmit}></Searchbar>
 
-        {this.state.content.length > 0 &&(
+        {content.length > 0 &&(
           <ImageGallery
-            onModalWindow={this.onModalWindow}>
+            onModalWindow={onModalWindow}>
 
             <ImageGalleryItem
-              content={this.state.content}></ImageGalleryItem>
+              content={content}></ImageGalleryItem>
 
           </ImageGallery>
         )}
 
-        {this.state.isLoad && (
+        {isLoad && (
           <ThreeDots
             wrapperStyle={{
               justifyContent: 'center',
             }}></ThreeDots>
         )}
 
-        {this.state.loadMore === true && (
+        {loadMore === true && (
           <ButtonLoadMore
-            loadMore={this.loadMore}></ButtonLoadMore>
+            loadMore={loadMorePage}></ButtonLoadMore>
         )}
-        {this.state.error && <div
+        {error && <div
           style={{ margin: 'auto' }}>
           ERROR CONNECTION PLEASE RELOAD THE PAGE</div>}
         
-        {this.state.notFound && <div
+        {notFound && <div
           style={{ margin: 'auto' }}>
           NOT FOUND</div>}
         
       </div>
-    );
-  }
+  )
+}
 
-};
+
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,36 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import { StyleModal } from "./Modal.styled";
 import { createPortal } from "react-dom";
 
 const modalRoot = document.querySelector('.Modal-root')
 
-export class Modal extends Component {
-
-    componentDidMount() {
-        window.addEventListener('keydown', this.handlerKeydown)
-    }
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handlerKeydown) 
-    }
-
-    handlerKeydown = (evt) => {
-        if (evt.code === 'Escape') {
-            this.props.closeModalWindow() 
-        }
-    }
-
-    handlerOverlay = (evt) => {
+export const Modal = ({ imgModal, closeModalWindow}) => {
+  
+    const handlerOverlay = (evt) => {
         if (evt.target.localName === 'div') {
-            this.props.closeModalWindow()
+            closeModalWindow()
         }
     }
+    useEffect(() => {
+        const handlerKeydown = (evt) => {
+            if (evt.code === 'Escape') {
+                closeModalWindow()
+            }
+        }
+        window.addEventListener('keydown', handlerKeydown)
+        return () => {
+            window.removeEventListener('keydown', handlerKeydown)
+        }
+    }, [closeModalWindow])
 
-    render() {
-        return createPortal(
-            <StyleModal className="overlay" onClick={this.handlerOverlay}>
-                    <div className="modal">
-                    <img src={this.props.imgModal} alt="" />
-                    </div>
-                </StyleModal>, modalRoot
-        )
-    }
+   
+
+
+    return createPortal(
+        <StyleModal className="overlay" onClick={handlerOverlay}>
+            <div className="modal">
+                <img src={imgModal} alt="" />
+            </div>
+        </StyleModal>, modalRoot
+  )
 }
